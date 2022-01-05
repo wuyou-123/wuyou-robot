1c3b4b7f72aaded83e58909ee049af291fcf1aa4
wuyou <1097810498@qq.com>
2022-01-05 10:45:35 +0800

    添加数据库链接

---
+++ src/main/java/pers/wuyou/robot/config/SshProperties.java
@@ -0,0 +1,37 @@
+package pers.wuyou.robot.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wuyou
+ */
+@Getter
+@Setter
+@ToString(exclude = "password")
+@ConfigurationProperties(prefix = "ssh")
+@Component
+public class SshProperties {
+
+    private String host;
+    private Integer port;
+    private String username;
+    private String password;
+    private Forward forward;
+
+    @Getter
+    @Setter
+    @ToString
+    public static class Forward {
+
+        private String fromHost;
+        private Integer fromPort;
+        private String toHost;
+        private Integer toPort;
+
+    }
+
+}
